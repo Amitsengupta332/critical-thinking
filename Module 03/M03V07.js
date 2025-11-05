7f0cb8d007ae18ca3f843ad9727b2af851c83454
amitsengupta <amitsengupta332@gmail.com>
2025-11-05 14:50:19 +0600

    Implement prepend and insert methods in LinkedList class; add comments for clarity

--- Module 03/M03V07.js
+++ Module 03/M03V07.js
@@ -1,3 +1,4 @@
+//  নোড তৈরি করলাম
 class Node {
   constructor(value) {
     this.value = value;
@@ -5,6 +6,7 @@ class Node {
   }
 }
 
+//  লিস্ট তৈরি করলাম 
 class LinkedList {
   constructor() {
     this.head = null;
@@ -31,13 +33,58 @@ class LinkedList {
     this.length++;
   }
 
-  prepend() {}
+  prepend(value) {
+    const newNode = new Node(value);
+    // if the list is empty
+    if (this.head === null) {
+      // this.head = {
+      //   value: value,
+      //   next: null,
+      // };
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // if the list is not empty
+      newNode.next = this.head;
+      this.head = newNode;
+    }
 
-  insert() {}
+    this.length++;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) {
+      console.error("Index out of bound ");
+
+      return undefined;
+    }
+    // if the insert is in the start of the linked list
+    if (index === 0) {
+      return this.prepend(value);
+    }
+
+    // if the insert is in the end of the list
+    if (index === this.length) {
+      return this.append(value);
+    }
+
+    // if the insert is in the middle
+
+    // find leading node
+    let count = 0;
+    let leadingNode = this.head;
+
+    while (count !== index - 1) {
+      leadingNode = leadingNode.next;
+      count++;
+    }
+
+    console.log(leadingNode)
+  }
 
   remove() {}
 
- print() {
+  print() {
     const arr = [];
     let currentNode = this.head;
 
@@ -54,6 +101,11 @@ const linkedList = new LinkedList();
 linkedList.append(10);
 linkedList.append(20);
 linkedList.append(30);
+
+linkedList.prepend(40);
+linkedList.prepend(50);
+linkedList.prepend(60);
+
+linkedList.insert(2, 100);
+
 linkedList.print();
-
-
